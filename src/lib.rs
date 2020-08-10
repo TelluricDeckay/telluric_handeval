c4b5ec52cdac7d7c5537d6ccb1ea495764db36e2
andy5995 <andy400-dev@yahoo.com>
2020-08-10 16:01:22 -0500

    switch to enum for PokerRanks

--- src/lib.rs
+++ src/lib.rs
@@ -1,46 +1,79 @@
 use ionic_deckhandler::{card_type, suit, Card, Deck};
 
-pub struct PokerRank(String);
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub enum PokerRankOrder {
+    Pair,
+    TwoPair,
+    ThreeOfAKind,
+    Straight,
+    Flush,
+    FullHouse,
+    FourOfAKind,
+    StraightFlush,
+    RoyalFlush,
+}
 
-const PAIR: i32 = 0;
-const TWO_PAIR: i32 = 1;
-const THREE_OF_A_KIND: i32 = 2;
-const STRAIGHT: i32 = 3;
-const FLUSH: i32 = 4;
-const FULL_HOUSE: i32 = 5;
-const FOUR_OF_A_KIND: i32 = 6;
-const STRAIGHT_FLUSH: i32 = 7;
-const ROYAL_FLUSH: i32 = 8;
+/*
+impl PokerRankOrder {
+    fn rank_value(&self) -> i32 {
+        match *self {
+            PokerRankOrder::Pair => 1,
+            PokerRankOrder::TwoPair => 2,
+            PokerRankOrder::ThreeOfAKind => 3,
+            PokerRankOrder::Straight => 4,
+            PokerRankOrder::Flush => 5,
+            PokerRankOrder::FullHouse => 6,
+            PokerRankOrder::FourOfAKind => 7,
+            PokerRankOrder::StraightFlush => 8,
+            PokerRankOrder::RoyalFlush => 9,
+        }
+    }
+} */
+
+/*
+impl PokerRank {
+    pub fn name(&self) -> &'static str {
+        match self {
+            Self::Pair => "Pair",
+            Self::TwoPair => "Two of a kind",
+            Self::ThreeOfAKind => "Three of a kind",
+            Self::Straight => "Straight",
+            Self::Flush => "Flush",
+            Self::FullHouse => "Full House",
+            Self::FourOfAKind => "Four of a kind",
+            Self::StraightFlush => "Straight Flush",
+            Self::RoyalFlush => "Royal Flush",
+        }
+    }
+}
+
+fn main() {
+    let my_rank = PokerRank::Pair;
+    println!("{}", my_rank.name());
+}
+*/
+
+struct PokerRank(&'static str);
 
 const NUM_OF_RANKS: i32 = 9;
 
 impl PokerRank {
-    pub fn new(lit: String) -> Self {
+    pub fn new(lit: &'static str) -> Self {
         Self { 0: lit }
     }
 
     pub fn init_ranks() -> [PokerRank; NUM_OF_RANKS as usize] {
         [
-            PokerRank::new(String::from("Pair")),
-            PokerRank::new(String::from("Two of a kind")),
-            PokerRank::new(String::from("Three of a kind")),
-            PokerRank::new(String::from("Straight")),
-            PokerRank::new(String::from("Flush")),
-            PokerRank::new(String::from("Full House")),
-            PokerRank::new(String::from("Four of a kind")),
-            PokerRank::new(String::from("Straight Flush")),
-            PokerRank::new(String::from("Royal Flush")),
+            PokerRank::new("Pair"),
+            PokerRank::new("Two of a kind"),
+            PokerRank::new("Three of a kind"),
+            PokerRank::new("Straight"),
+            PokerRank::new("Flush"),
+            PokerRank::new("Full House"),
+            PokerRank::new("Four of a kind"),
+            PokerRank::new("Straight Flush"),
+            PokerRank::new("Royal Flush"),
         ]
-    }
-}
-
-/* Compiles. prevents repetitive code, but isn't needed because hand is already
- * an array. Maybe this will give you ideas though. */
-pub struct Hand([Card; 5]);
-
-impl Hand {
-    pub fn new(hand: &[Card; 5]) -> Self {
-        Self { 0: *hand }
     }
 }
 
@@ -53,7 +86,8 @@ pub fn evaluate(hand: &[Card; 5]) -> i32 {
 
 #[test]
 fn test_evaluate() {
-    let hand_arr: [Card; 5] = [
+    #[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+    let mut hand_arr: [Card; 5] = [
         Card::new(&suit::CLUB, &card_type::ACE),
         Card::new(&suit::HEART, &card_type::THREE),
         Card::new(&suit::DIAMOND, &card_type::THREE),
@@ -61,14 +95,15 @@ fn test_evaluate() {
         Card::new(&suit::CLUB, &card_type::QUEEN),
     ];
 
-    let st_hand = Hand::new(&hand_arr);
-    for card in 0..5 {
-        println!("{}", st_hand.0[card].value);
-    }
+    //
+    // Quicksort functions
+    // https://turreta.com/2019/10/22/quicksort-algorithm-example-in-rust/
+    // let last_index = hand_array.len() - 1;
 
     println!();
     println!("{}", hand_arr[2].value);
-    assert!(true)
+    println!();
+    assert!(PokerRankOrder::Flush == PokerRankOrder::Flush)
 }
 
 #[cfg(test)]
