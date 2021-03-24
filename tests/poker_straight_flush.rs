2652266fd7e5342e4aecc20bca32a8d8d59d2748
andy5995 <andy400-dev@yahoo.com>
2021-03-24 12:42:01 -0500

    add comparator tests for straight flush
    closes #8

--- tests/poker_straight_flush.rs
+++ tests/poker_straight_flush.rs
@@ -1,9 +1,10 @@
 use ionic_deckhandler::{Card, Rank, Suit};
+use std::cmp::Ordering;
 use telluric_handeval::poker::{HandRank, PokerRankedHand};
 
 #[test]
 fn test_evaluate_straight_flush() {
-    let hand_arr: [Card; 5] = [
+    let hand_arr1: [Card; 5] = [
         Card::new(Rank::Queen, Suit::Clubs),
         Card::new(Rank::King, Suit::Clubs),
         Card::new(Rank::Nine, Suit::Clubs),
@@ -11,14 +12,14 @@ fn test_evaluate_straight_flush() {
         Card::new(Rank::Jack, Suit::Clubs),
     ];
     assert_eq!(
-        hand_arr.evaluate_hand(),
+        hand_arr1.evaluate_hand(),
         HandRank::StraightFlush {
             highest_rank: Rank::King
         }
     );
 
     // Test for Ace-low Straight Flush
-    let hand_arr = [
+    let hand_arr2 = [
         Card::new(Rank::Five, Suit::Hearts),
         Card::new(Rank::Three, Suit::Hearts),
         Card::new(Rank::Two, Suit::Hearts),
@@ -26,19 +27,53 @@ fn test_evaluate_straight_flush() {
         Card::new(Rank::Four, Suit::Hearts),
     ];
     assert_eq!(
-        hand_arr.evaluate_hand(),
+        hand_arr2.evaluate_hand(),
         HandRank::StraightFlush {
             highest_rank: Rank::Five
         }
     );
 
+    // A straight flush should never be considered a straight
+    let hand_arr3 = [
+        Card::new(Rank::Five, Suit::Clubs),
+        Card::new(Rank::Six, Suit::Clubs),
+        Card::new(Rank::Seven, Suit::Clubs),
+        Card::new(Rank::Eight, Suit::Clubs),
+        Card::new(Rank::Nine, Suit::Clubs),
+    ];
+    assert!(
+        hand_arr3.evaluate_hand()
+            != HandRank::Straight {
+                highest_rank: Rank::Nine
+            }
+    );
+
     // Test for Royal Flush (Ace-high straight flush)
-    let hand_arr = [
+    let hand_arr4 = [
         Card::new(Rank::Queen, Suit::Hearts),
         Card::new(Rank::Ten, Suit::Hearts),
         Card::new(Rank::Jack, Suit::Hearts),
         Card::new(Rank::Ace, Suit::Hearts),
         Card::new(Rank::King, Suit::Hearts),
     ];
-    assert_eq!(hand_arr.evaluate_hand(), HandRank::RoyalFlush);
+    assert_eq!(hand_arr4.evaluate_hand(), HandRank::RoyalFlush);
+
+    // Comparator tests.
+    assert_eq!(
+        hand_arr1.evaluate_hand().cmp(&hand_arr4.evaluate_hand()),
+        Ordering::Less
+    );
+
+    let hand_arr5: [Card; 5] = [
+        Card::new(Rank::Queen, Suit::Clubs),
+        Card::new(Rank::Ace, Suit::Hearts),
+        Card::new(Rank::Ace, Suit::Diamonds),
+        Card::new(Rank::Ace, Suit::Clubs),
+        Card::new(Rank::Queen, Suit::Clubs),
+    ];
+
+    assert_eq!(
+        hand_arr1.evaluate_hand().cmp(&hand_arr5.evaluate_hand()),
+        Ordering::Greater
+    );
 }
